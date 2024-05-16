aa36b3c79e02bdaec38d091c6118423061324687
AdrianaLopez27 <adrilopezcalderon72@gmail.com>
2024-05-16 03:11:03 -0600

    extract data from Pokemon API

--- pokemon-app/src/components/screen/Card.js
+++ pokemon-app/src/components/screen/Card.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Card extends Component {
+  state = {
+    name: '',
+    Iurl: '',
+    pokemonId: ''
+  };
+  
+  
+  
   render() {
+
+    const name = this.props.name;
+    const url = this.props.url;
+    
     return (
       <div class="col-md-4 col-sm-6 mb-5">
             <div class='card'>
                 <div class='header_card'>
                     <h1>
-                        Pokemon Name
+                        {name}
                     </h1>
 
                 </div>  
--- pokemon-app/src/components/screen/PokemonPool.js
+++ pokemon-app/src/components/screen/PokemonPool.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import Card from './Card'
 
 export default class PokemonPool extends Component {
+  state={
+    url:'https://pokeapi.co/api/v2/pokemon/',
+    pokemon: null
+  }
+  async componentDidMount(){
+    const res = await axios.get(this.state.url);
+    this.setState({pokemon: res.data['results']});
+  }
+  
+  
   render() {
     return (
-      <div class='row'>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+      <div>
+        {this.state.pokemon ? (
+          <div class='row'>
+              {this.state.pokemon.map(pokemon => (
+                <Card
+                  key={pokemon.name}
+                  name={pokemon.name}
+                  url={pokemon.url}
+                />
+              ))}
+          </div>
+        ) : (
+          <h3>Waiting... Pokemons are loading</h3>
+          
+        )}
+          
       </div>
-    )
+        
+    );
   }
 }
