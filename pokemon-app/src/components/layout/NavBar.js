1093f8f60d109fd5f7b6be27d6a0cb0bc2d7b9b7
AdrianaLopez27 <adrilopezcalderon72@gmail.com>
2024-05-15 23:21:55 -0600

    adding navbar component

--- pokemon-app/src/components/layout/NavBar.js
+++ pokemon-app/src/components/layout/NavBar.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import logoImage from '../../assets/logoPokedex.png'
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar bg-dark fixed-top">
+        <nav class="navbar bg-danger fixed-top">
             <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">POKEDEX</span>
+                <a class="navbar-brand" href="#">
+                    <img src={logoImage} width="120" height="auto"></img>
+                </a>
+                
             </div>
         </nav>
       </div>
