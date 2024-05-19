c4017f4c93f0245d186d938db4bc4accdc9b7e12
AdrianaLopez27 <adrilopezcalderon72@gmail.com>
2024-05-19 04:53:41 -0600

    adding styling to the create and edit forms

--- pokemon-app/src/components/screen/CreatePokemon.js
+++ pokemon-app/src/components/screen/CreatePokemon.js
@@ -54,48 +54,65 @@ class CreatePokemon extends Component {
     const { name, height, weight, abilities } = this.state;
 
     return (
-      <div>
-        <h2>Create New Pokémon</h2>
-        <form>
-          <div>
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleInputChange}
-            />
+      <div class='card  rounded-2'>
+        <div class='card-header bg-primary rounded-2 rounded-bottom-0'>
+            <h2 class='card-title text-center'>Create New Pokémon</h2>
+        </div>
+        <form >
+          <div class='row mx-auto p-4'>  
+            <div class="col-7">
+                <label>Name:</label>
+            </div> 
+            <div class="col-5"> 
+                <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleInputChange}
+                />
+            </div>
+
+            <div class="col-7">
+                
+                    <label>Height (cm):</label>
+            </div> 
+            <div class="col-5"> 
+                <input
+                type="number"
+                name="height"
+                value={height}
+                onChange={this.handleInputChange}
+                />
+            </div>
+            
+            <div class="col-7">
+                    <label>Weight (g):</label>
+            </div> 
+            <div class="col-5"> 
+                <input
+                type="number"
+                name="weight"
+                value={weight}
+                onChange={this.handleInputChange}
+                />
+            </div>
+            <div class="col-7">
+                    <label>Abilities:</label>
+            </div> 
+            <div class="col-5">
+                <input
+                type="text"
+                name="abilities"
+                value={abilities}
+                onChange={this.handleInputChange}
+                />
+            </div>
           </div>
-          <div>
-            <label>Height (cm):</label>
-            <input
-              type="number"
-              name="height"
-              value={height}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Weight (g):</label>
-            <input
-              type="number"
-              name="weight"
-              value={weight}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Abilities:</label>
-            <input
-              type="text"
-              name="abilities"
-              value={abilities}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <button type="button" onClick={this.handleSave}>
-            Save
-          </button>
+            <div class="d-flex justify-content-center">
+                <button className="btn btn-danger my-4" type="button" onClick={this.handleSave}>
+                    Save
+                </button>
+            </div>
         </form>
       </div>
     );
--- pokemon-app/src/components/screen/EditPokemon.js
+++ pokemon-app/src/components/screen/EditPokemon.js
@@ -65,39 +65,51 @@ class EditPokemon extends Component {
     const { name, height, weight } = this.state;
 
     return (
-      <div>
-        <h2>Edit Pokémon</h2>
+      <div class='card  rounded-2'>
+        <div class='card-header bg-primary rounded-2 rounded-bottom-0'>
+          <h2 class='card-title text-center'>Edit Pokémon</h2>
+        </div>
         <form>
-          <div>
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleInputChange}
-            />
+          <div class='row mx-auto p-4'>
+            <div class="col-7">
+              <label>Name:</label>
+            </div> 
+            <div class="col-5"> 
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div class="col-7">
+              <label>Height (cm):</label>
+            </div> 
+            <div class="col-5">
+              <input
+                type="number"
+                name="height"
+                value={height}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div class="col-7">
+              <label>Weight (g):</label>
+            </div> 
+            <div class="col-5">
+              <input
+                type="number"
+                name="weight"
+                value={weight}
+                onChange={this.handleInputChange}
+              />
+            </div>
           </div>
-          <div>
-            <label>Height (cm):</label>
-            <input
-              type="number"
-              name="height"
-              value={height}
-              onChange={this.handleInputChange}
-            />
+          <div class="d-flex justify-content-center">  
+              <button className="btn btn-danger my-4" type="button" onClick={this.handleSave}>
+                Save
+              </button>
           </div>
-          <div>
-            <label>Weight (g):</label>
-            <input
-              type="number"
-              name="weight"
-              value={weight}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <button type="button" onClick={this.handleSave}>
-            Save
-          </button>
         </form>
       </div>
     );
